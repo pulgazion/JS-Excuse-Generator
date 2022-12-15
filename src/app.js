c113dc85531d8c2ef85f4c1991a75386338dba2e
pulgazion <reimarrodri@gmail.com>
2022-12-15 17:11:40 +0000

    Mi primer excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = randomNumber();
+  console.log("Hello Rigo from the console! " + randomNumber);
 };
+
+let;
