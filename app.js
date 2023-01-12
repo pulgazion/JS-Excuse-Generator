98987d9ac20c727a27dd626521b7f97832fba919
pulgazion <reimarrodri@gmail.com>
2023-01-12 00:33:06 +0000

    Updates

---
+++ app.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import "./style.css";
+
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  console.log("Hello Rigo from the console! ");
+};
+
+let generateExcuse = () => {
+  let pronoum = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "druver", "comedian", "piincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+};
